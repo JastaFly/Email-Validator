1e42d4ef5edcdb9ad339df1ced39fa9573077517
zumawuu <zumawuu@gmail.com>
2022-05-06 21:32:46 +0300

    Add email validator

---
+++ EmailValidator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace VMeleshkin\Validators;
+
+class MailValidator
+{
+    private $emailValidRegExp = '/^(([\wА-Яа-я]{1,})@([\wА-Яа-я]{1,}\.)+[A-Za-zА-Яа-я]{2,})$/ui';
+    private $emailAddress;
+
+    public function validateEmail(string $email)
+    {
+        $validateResult = $this->validate($email);
+
+        if ($validateResult == '') {
+            throw new \Exception('DATA_NO_GENERATED');
+        }
+
+        return $validateResult;
+    }
+
+    public function validate(string $email): string
+    {
+        if ($email == '') {
+            return 'EMPTY_INPUT';
+        }
+
+        $this->emailAddress = $email;
+
+        if ($this->validateEmailAddress()) {
+
+            if ($this->checkMXRecord()) {
+                return 'EMAIL_OK';
+            } else {
+                return 'EMAIL_MX_FAILED';
+            }
+        }
+
+        return 'EMAIL_VALID_FAILED';
+    }
+
+    private function validateEmailAddress()
+    {
+        return preg_match($this->emailValidRegExp, $this->emailAddress);
+    }
+
+    private function checkMXRecord(): bool
+    {
+        $arMailAddress = explode('@', $this->emailAddress);
+
+        return getmxrr($arMailAddress[1], $hosts);
+    }
+}
